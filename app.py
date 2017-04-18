c1420393bd1be45a2d43cfb41f5896e2e4d0444d
indi4u <indi4u@gmail.com>
2017-04-18 10:54:55 +0530

    hosting address / port changes

--- app.py
+++ app.py
@@ -224,8 +224,8 @@ def tts():
 
 # In[ ]:
 
-run(host='localhost', port=8080, debug=True)
-
+#run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 # In[ ]:
 
