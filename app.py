575e0030e548e8d462abc36a3bbcdf5eef774f9c
indi4u <indi4u@gmail.com>
2017-04-20 17:31:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 # coding: utf-8
 # Computer Vision API
 from __future__ import print_function
-import site
 import bing_voice
 from bing_voice import *
 import pyaudio
 import sys
-
 import wave
 import os
 from bottle import route, run
